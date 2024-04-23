6143dc02145bf42eaa28546929d883f6b65d68f7
faisalyudiansah <mfaisal.yudiansah@gmail.com>
2024-04-23 15:27:48 +0700

    fix: fixed date

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
     <>
       <footer className="footer footer-center p-10 bg-base-200 text-base-content rounded">
         <aside>
-          <p>Muhammad Faisal Yudiansah - Pengerjaan : 22 April 2024 - 24 April 2024</p>
+          <p>Muhammad Faisal Yudiansah - Pengerjaan : 22 April 2024 - 23 April 2024</p>
         </aside>
       </footer>
     </>
